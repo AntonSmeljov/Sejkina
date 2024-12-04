3610712865287a52cd780eb180d8eff6e4c91270
AntonSmeljov <antonsmeljov@gmail.com>
2024-12-04 12:12:45 +0100

    Commit

---
+++ data.js
@@ -0,0 +1,363 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dnevna-soba",
+      "name": "Dnevna soba",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.7948912492823332,
+        "pitch": -0.020544971323946726,
+        "fov": 1.7428903542066503
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4061124321294347,
+          "pitch": 0.1256635224022915,
+          "rotation": 0,
+          "target": "5-dvorite-1"
+        },
+        {
+          "yaw": 0.8520990222677938,
+          "pitch": 0.19406336538088453,
+          "rotation": 0,
+          "target": "4-hodnik-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-spavaa-soba",
+      "name": "Spavaća soba",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 3.0350698149941966,
+        "pitch": -0.015111410794403923,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.94878334807491,
+          "pitch": 0.12677817474995656,
+          "rotation": 0,
+          "target": "6-dvorite-2"
+        },
+        {
+          "yaw": 0.15324355965697833,
+          "pitch": 0.27126921009045724,
+          "rotation": 0,
+          "target": "4-hodnik-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-hodnik-1",
+      "name": "Hodnik 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.39674371825901567,
+        "pitch": 0.02077163870019305,
+        "fov": 1.9736325747279384
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6681644191785878,
+          "pitch": 0.2611713468249306,
+          "rotation": 0,
+          "target": "3-kupatilo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-kupatilo",
+      "name": "Kupatilo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.42311199341535577,
+        "pitch": 0.07398895176992681,
+        "fov": 1.7055285299772716
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.553356751125186,
+          "pitch": 0.15214452277338353,
+          "rotation": 0,
+          "target": "4-hodnik-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-hodnik-2",
+      "name": "Hodnik 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.2740380638580486,
+        "pitch": 0.04762986908113476,
+        "fov": 1.877246379607182
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.769898079774661,
+          "pitch": 0.14298217690807746,
+          "rotation": 0,
+          "target": "1-spavaa-soba"
+        },
+        {
+          "yaw": -1.4917356922254577,
+          "pitch": 0.1833998798574168,
+          "rotation": 0,
+          "target": "2-hodnik-1"
+        },
+        {
+          "yaw": -2.028661613877123,
+          "pitch": 0.17834709157670403,
+          "rotation": 0,
+          "target": "3-kupatilo"
+        },
+        {
+          "yaw": 2.8741311083765204,
+          "pitch": 0.4346983376710778,
+          "rotation": 0,
+          "target": "0-dnevna-soba"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dvorite-1",
+      "name": "Dvorište 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.05189110424217169,
+        "pitch": -0.039603271361562165,
+        "fov": 1.7917512324913103
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.47400230330441495,
+          "pitch": 0.1292620998566889,
+          "rotation": 0,
+          "target": "0-dnevna-soba"
+        },
+        {
+          "yaw": 0.7571986319838722,
+          "pitch": 0.2137605382529486,
+          "rotation": 0,
+          "target": "6-dvorite-2"
+        },
+        {
+          "yaw": -0.8002926589111468,
+          "pitch": 0.13127546018625047,
+          "rotation": 0,
+          "target": "7-dvorite-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-dvorite-2",
+      "name": "Dvorište 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.16500027321202992,
+        "pitch": 0.0017732458304795529,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1170453327353282,
+          "pitch": 0.16775834222419128,
+          "rotation": 0,
+          "target": "1-spavaa-soba"
+        },
+        {
+          "yaw": 0.02320651893454695,
+          "pitch": 0.1673252357163424,
+          "rotation": 0,
+          "target": "5-dvorite-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-dvorite-3",
+      "name": "Dvorište 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.17117203537742753,
+          "pitch": 0.16924340814139782,
+          "rotation": 0,
+          "target": "6-dvorite-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Šejkina",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
